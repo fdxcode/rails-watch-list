f7c1053ad17b127abc8ee5998b1d7c78a98bb364
Fernando Sapene <fernandosapene13@gmail.com>
2021-10-24 18:34:19 -0400

    Seed peliculas agragadas

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,51 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Movie.create( title: "Ocean's Eight",
-              overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.",
-              movie_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg",
-              trailer_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg",
-              poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg",
-              rating: 7.0)
+Movie.create(title: "Bienvenido al mundo de Los Ogglies",
+             overview: "Los Ogglies buscan un nuevo hogar. Apestan y la mayoría de los humanos no los quieren. Cuando Firebottom, el dragón de la familia, aterriza en el vertedero de Smelliville, los Ogglies se sienten como en casa.",
+             movie_url: "https://www.youtube.com/embed/95r7VsNqz6k",
+             trailer_url: "https://www.youtube.com/embed/xmQbj9x3boE",
+             poster_url: "https://is4-ssl.mzstatic.com/image/thumb/Video115/v4/32/e1/66/32e1663a-a137-a68a-2d48-79a9e9bbb014/TheOgglies_ART_16x9_es-419_iTunes.jpg/1200x675.jpg",
+             rating: 3.0)
+
+Movie.create(title: "La leyenda del gigante de la montaña",
+             overview: "Los Ogglies buscan un nuevo hogar. Apestan y la mayoría de los humanos no los quieren. Cuando Firebottom, el dragón de la familia, aterriza en el vertedero de Smelliville, los Ogglies se sienten como en casa.",
+             movie_url: "https://www.youtube.com/embed/k6uzLQuBLQc",
+             trailer_url: "https://www.youtube.com/embed/BzeSt_87whk",
+             poster_url: "https://spotlight.cdn.telefonica.com/customer/v1/source?image=http:%2F%2Fmedia.gvp.telefonica.com%2Fstoragearea0%2FIMAGES%2F00%2F06%2F71%2F6714497_D082EC70921F5284.jpg&width=1400&height=787&resize=CROP&format=JPEG",
+             rating: 3.5)
+
+Movie.create(title: "2067 ",
+             overview: "En el futuro cercano, el cambio climático ha provocado la necesidad de emplear oxígeno artificial para poder respirar. Con su mujer enferma y pocas esperanzas de sobrevivir, Ethan (Smit-McPhee) se ve obligado a viajar al futuro para buscar una cura para su misteriosa enfermedad. Sin saber en quién confiar, debe navegar por un futuro repleto de peligros y sin la certeza de poder regresar a casa.",
+             movie_url: "https://www.youtube.com/embed/ySQpJC6HKe0?controls=0&amp;start=2",
+             trailer_url: "https://www.youtube.com/embed/BzeSt_87whk",
+             poster_url: "https://th.bing.com/th/id/R.15b63300553157c6dbf08c6935ad77be?rik=%2bFTaZMRKYyWAvw&riu=http%3a%2f%2fhorrorfuel.com%2fwp-content%2fuploads%2f2020%2f10%2f2067-a-e1602717334336.jpeg&ehk=0spaTXC2S05EE%2fO4htg89gHI4wN9%2fxO7aE7Ib4saHnM%3d&risl=&pid=ImgRaw&r=0",
+             rating: 4.6)
+
+Movie.create(title: "Deadpool 2",
+             overview: "El mercenario mutante malhablado Wade Wilson (también conocido como Deadpool), reúne a un equipo de compañeros mutantes para proteger a un joven con habilidades sobrenaturales del brutal cíborg Cable, que viaja en el tiempo.",
+             movie_url: "https://www.youtube.com/embed/I9Kz_3fsN7Y?controls=0&amp;start=2",
+             trailer_url: "https://www.youtube.com/embed/U6VMFwS2mPk?controls=0&amp;start=2",
+             poster_url: "https://3.bp.blogspot.com/-aQKDS8F_yq4/W48SvNTp2kI/AAAAAAAABv0/qdCm_rIipaUgYGrjKACSHL3XXDrGcGeUgCLcBGAs/s1600/deadpool_2_poster_by_punktx30-db2iiu7.jpg",
+             rating: 5.5)
+
+Movie.create(title: "Un jefe en pañales",
+             overview: "The Boss Baby (titulada Un jefe en pañales en Hispanoamérica y Bebé jefazo en España) es una película de comedia animada por computadora 3D de 2017, basada en el libro ilustrado de 2010 del mismo nombre escrito e ilustrado por Marla Frazee. La película fue producida por DreamWorks Animation y dirigida por Tom McGrath y escrita por Michael McCullers. Las voces son puestas por Alec Baldwin, Miles Christopher Bakshi, Steve Buscemi, Jimmy Kimmel, Lisa Kudrow y Tobey Maguire. La trama sigue a un bebé que es un agente secreto en la guerra secreta entre bebés y perritos.",
+             movie_url: "https://www.youtube.com/embed/92oBrbk-Lgs?controls=0&amp;start=2",
+             trailer_url: "https://www.youtube.com/embed/SXpVk9uKL8I?controls=0&amp;start=2",
+             poster_url: "https://2.bp.blogspot.com/-lIx0cbll84g/WTIgpBADtpI/AAAAAAAALNA/H3nkyqaZm3kr4SnNIW1VLY5BwnyrgXZNgCLcB/s1600/un-jefe-en-pa%25C3%25B1ales.jpg",
+             rating: 3.5)
+
+Movie.create(title: "Venom",
+             overview: "The Boss Baby (titulada Un jefe en pañales en Hispanoamérica y Bebé jefazo en España) es una película de comedia animada por computadora 3D de 2017, basada en el libro ilustrado de 2010 del mismo nombre escrito e ilustrado por Marla Frazee. La película fue producida por DreamWorks Animation y dirigida por Tom McGrath y escrita por Michael McCullers. Las voces son puestas por Alec Baldwin, Miles Christopher Bakshi, Steve Buscemi, Jimmy Kimmel, Lisa Kudrow y Tobey Maguire. La trama sigue a un bebé que es un agente secreto en la guerra secreta entre bebés y perritos.",
+             movie_url: "https://www.youtube.com/embed/XF-xyQxT2Dw?controls=0&amp;start=2",
+             trailer_url: "https://www.youtube.com/embed/F4Ygcigj0Gk?controls=0&amp;start=2",
+             poster_url: "https://th.bing.com/th/id/R.d4b4406499773c9e8bca36bb97347806?rik=mWgABoQJsNoC6A&pid=ImgRaw&r=0",
+             rating: 6.5)
+
+Movie.create(title: "S.W.A.T",
+             overview: "S.W.A.T. es una película de acción, policíaco y crimen de 2003 basada en la serie de televisión del mismo nombre de 1975 de Robert Hamner y Lee Stanley. Fue dirigida por Clark Johnson y protagonizada por Samuel L. Jackson, Colin Farrell y Michelle Rodríguez. Su lanzamiento en los Estados Unidos fue el 8 de agosto de 2003.",
+             movie_url: "https://www.youtube.com/embed/QG8YiBOVq6k?controls=0&amp;start=2",
+             trailer_url: "https://www.youtube.com/embed/vr_N2uewo20?controls=0&amp;start=2",
+             poster_url: "https://aquipelis.net/wp-content/uploads/2014/01/s_w_a_t_swat-945799030-large.jpg",
+             rating: 3.7)
